3e46fb0b1449349b94d0a592f2a2fcc94beb1771
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-03-24 17:20:42 +0000

    save work

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 kyle.matthew.magnaye All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kyle Matthew Magnaye
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
 /**
- * Displays an alert with the message "Hello, World!"
+ * Calculates the volume of a cube. "
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function calculateVolume() {
+  // input
+  const sideLength = parseFloat(document.getElementById("side-length").value)
+
+  // process
+  if (!isNaN(sideLength) && sideLength > 0) {
+    const volume = Math.pow(sideLength, 3)
+    // output
+    document.getElementById("cube-volume").innerHTML =
+      "The volume of the cube is: " + volume.toFixed(2) + " cubic units."
+  } else {
+    document.getElementById("cube-volume").innerHTML = "Please enter a valid positive number."
+  }
 }
